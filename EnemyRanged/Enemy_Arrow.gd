b0307182f8a5fff0d8b481065634b8302132cbfa
Fayiz <fayizasyrofi18@gmail.com>
2026-02-09 21:29:00 +0700

    nambahin health system untuk player

--- EnemyRanged/Enemy_Arrow.gd
+++ EnemyRanged/Enemy_Arrow.gd
@@ -2,17 +2,21 @@ extends Area2D
 
 const speed = 400
 var direction = Vector2.ZERO
+var damage = 20
+
+func _ready():
+	body_entered.connect(_on_body_entered)
 
 func _physics_process(delta: float) -> void:
 	if direction != Vector2.ZERO:
 		position += direction * speed * delta
-	pass
 
 func set_direction(dir_input: Vector2):
 	direction = dir_input
-	rotation = direction.angle() #otomatis ngitung sudut
-	
-	#if direction.x < 0:
-		#rotation_degrees = 180
-	#else :
-		#rotation_degrees = 0
+	rotation = direction.angle()
+
+func _on_body_entered(body):
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+
+	queue_free()
